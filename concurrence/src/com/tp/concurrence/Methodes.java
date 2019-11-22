1a6aed35d754aa6e85dae1f5f2be6d8d6dcd8fd4
haroldconan <Avril 99>
2019-11-22 21:34:17 +0100

    hc : mise en place de ant

--- concurrence/src/com/tp/concurrence/Methodes.java
+++ concurrence/src/com/tp/concurrence/Methodes.java
@@ -1,4 +1,4 @@
-package concurrence;
+package com.tp.concurrence;
 
 import java.util.HashSet;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
--- concurrence/src/com/tp/concurrence/Metier.java
+++ concurrence/src/com/tp/concurrence/Metier.java
@@ -1,4 +1,4 @@
-package concurrence;
+package com.tp.concurrence;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
